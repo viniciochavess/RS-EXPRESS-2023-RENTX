9c6640cbb1acd68dd07bfe4163385f8dc24650d6
Vinicio Chaves <47008512+viniciochavess@users.noreply.github.com>
2025-05-29 17:40:55 -0300

    task: configuração typeorm e o docker postgres

---
+++ src/database/index.ts
@@ -0,0 +1,13 @@
+import { DataSource } from 'typeorm';
+
+export const AppDataSource = new DataSource({
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'rentx',
+  password: 'rentx',
+  database: 'rentx',
+  entities: ['./src/modules/**/entities/*.ts'],
+  migrations: ['./src/database/migrations/*.ts'],
+  synchronize: false
+});
