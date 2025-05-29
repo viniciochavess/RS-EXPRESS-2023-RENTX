3d639dde4e3c7129f199fd1be147b4a5ffa846d1
Vinicio Chaves <47008512+viniciochavess@users.noreply.github.com>
2025-05-29 16:35:04 -0300

    task: configuração do swagger documentação

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,14 @@
 import express from "express";
 import { categoriesRoutes } from "./routes/categories.routes";
 import { specificationRoutes } from "./routes/specifications.routes";
+import swaggerUi from 'swagger-ui-express'
+import swaggerFile from './swagger.json'
 const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded())
 
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile))
 app.use('/categories',categoriesRoutes)
 app.use('/specifications', specificationRoutes)
 
